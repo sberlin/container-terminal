74a3ab31bc1e4ea78647a1c382bb2fe151bc6fd2
sberlin <sberlin@users.noreply.github.com>
2019-02-22 17:53:51 +0100

    Revert "Upload file as chunks"
    This reverts commit d212dde2acb7feb5c642b445e6e23891676434c6.

--- container-terminal.js
+++ container-terminal.js
@@ -84,7 +84,7 @@
                         screenKeys: '=?',
                         autofocus: '=?',
                         status: '=?',
-                        fileUpload: '=?'
+                        file: '=?'
                     },
                     link: function(scope, element, attrs) {
                         scope.status = 'disconnected';
@@ -275,18 +275,11 @@
                                 connect();
                         });
 
-                        scope.$watch("fileUpload", function(fileUpload) {
-                            if (fileUpload && term) {
-                                const file = fileUpload.file;
-                                const chunkSize = fileUpload.chunkSize || 1000000;
-                                const chunkOffset = (fileUpload.chunkOffset || 0) * chunkSize;
+                        scope.$watch("file", function(file) {
+                            if (file && term) {
                                 var reader = new FileReader();
                                 reader.onload = function(){
-                                    const length = reader.result.length;
-                                    var chunkCount = Number.parseInt(length / chunkSize) + 1;
-                                    for (var i=chunkOffset; i<length; i+=chunkSize) {
-                                        term.emit("data", `echo -n -e >> /tmp/${file.name}-${i} "$(cat <<-EOC\n` + reader.result.slice(i, i+chunkSize) + "\nEOC\n)\"\n");
-                                    }
+                                    term.emit("data", reader.result);
                                 };
                                 reader.readAsText(file);
                             }
